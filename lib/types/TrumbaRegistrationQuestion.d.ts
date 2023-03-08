12d6576bb61a471a682fb54c21d2c1f608054b7f
Yuen Ler Chow <lerchow@gmail.com>
2023-03-08 12:23:11 -0500

    build

---
+++ lib/types/TrumbaRegistrationQuestion.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Trumba registration question for when registering for an event
+ * @author Yuen Ler Chow
+ */
+declare type TrumbaRegistrationQuestion = {
+    fieldID: number;
+    question: string;
+    fieldType: number;
+    required: boolean;
+    defaultValue: string[];
+    allowedValues: string[];
+    allowsMultipleValues: boolean;
+    validationRegex: string;
+    conditionalFieldID: number;
+    conditionalValues: string[];
+};
+export default TrumbaRegistrationQuestion;
